aba74be2abda0ae33cc4e67cb93f15fbe69ff5b2
HatemRamadan <hatemram96@gmail.com>
2025-02-16 00:31:14 +0200

    add token bucket algorithm

--- index.js
+++ index.js
@@ -9,6 +9,9 @@ const WINDOW_SIZE = 60000;
 // Number of allowed requests per window interval
 const RATE_LIMIT = 3;
 
+const REFILL_RATE_PER_SECOND = 0.2;
+const MAXIMUM_BUCKET_SIZE = 3;
+
 app.get('/fixed-window', async (req, res) => {
   const client = await redis.createClient()
   .on('error', err => console.log('Redis Client Error', err))
@@ -52,6 +55,38 @@ app.get('/sliding-window', async (req, res) => {
     // Add the current request
     requestsWithinWindow.push(currentRequestTimestamp);
     await client.set(`${userIdentifier}`, JSON.stringify(requestsWithinWindow));
+  }
+
+  res.send(`Hello World!`);
+});
+
+app.get('/token-bucket', async (req, res) => {
+  const client = await redis.createClient()
+  .on('error', err => console.log('Redis Client Error', err))
+  .connect();
+
+  // Identifier for the user; can be IP or GeoLocation or email
+  const userIdentifier = req.ip;
+  const lastRefill = await client.get(`${userIdentifier}:lastRefill`);
+  const count = await client.get(`${userIdentifier}:count`);
+  
+  // Initialize the last bucket refill to now and count to maximum bucket size
+  const currentTime = Math.round(Date.now()/1000);
+  if (!lastRefill) {
+    await client.set(`${userIdentifier}:count`, MAXIMUM_BUCKET_SIZE - 1);
+    await client.set(`${userIdentifier}:lastRefill`, currentTime);
+    return res.send(`Hello World!`);
+  }
+
+  // Calculate the number of the tokens in the bucket according to the refill rate
+  const secondsSinceLastRefill = currentTime - lastRefill;
+  const newCount = Math.min(Number(count) + Math.round(secondsSinceLastRefill * REFILL_RATE_PER_SECOND), MAXIMUM_BUCKET_SIZE);
+
+  // Remove a token for the current request
+  await client.set(`${userIdentifier}:count`, Math.max(newCount - 1, 0));
+  await client.set(`${userIdentifier}:lastRefill`, currentTime);
+  if (newCount === 0) {
+    return res.status(429).send('Too many requests');
   }
 
   res.send(`Hello World!`);

0b5ca70c214bcdb0a9ebc283c482e2d6b8f130ee
HatemRamadan <hatemram96@gmail.com>
2025-02-16 00:06:51 +0200

    add sliding window algorithm

--- index.js
+++ index.js
@@ -14,15 +14,46 @@ app.get('/fixed-window', async (req, res) => {
   .on('error', err => console.log('Redis Client Error', err))
   .connect();
 
-  // Identifier for the user can be IP or GeoLocation or email
-  const userIp = req.ip;
+  // Identifier for the user can be IP or geoLocation or email
+  const userIdentifier = req.ip;
   const window = Math.floor(Date.now()/WINDOW_SIZE);
-  const counter = await client.get(`${userIp}:${window}`);
+  const counter = await client.get(`${userIdentifier}:${window}`);
   if (counter > RATE_LIMIT) {
     return res.status(429).send('Too many requests');
   }
 
-  await client.incr(`${userIp}:${window}`);
+  await client.incr(`${userIdentifier}:${window}`);
+  res.send(`Hello World!`);
+});
+
+
+// Filter timestamps within the current window period
+function filterRequestsWithinWindow(timestamps, currentRequestTimestamp) {
+  const filteredTimestamps = timestamps.filter(timestamp => timestamp > currentRequestTimestamp - WINDOW_SIZE);
+  return filteredTimestamps;
+}
+
+app.get('/sliding-window', async (req, res) => {
+  const client = await redis.createClient()
+  .on('error', err => console.log('Redis Client Error', err))
+  .connect();
+
+  // Identifier for the user; can be IP or GeoLocation or email
+  const userIdentifier = req.ip;
+  const previousRequests = await client.get(`${userIdentifier}`);
+
+  if (previousRequests) {
+    const currentRequestTimestamp = Date.now();
+    const requestsWithinWindow = filterRequestsWithinWindow(JSON.parse(previousRequests), currentRequestTimestamp);
+    if (requestsWithinWindow.length > RATE_LIMIT) {
+      return res.status(429).send('Too many requests'); 
+    }
+  
+    // Add the current request
+    requestsWithinWindow.push(currentRequestTimestamp);
+    await client.set(`${userIdentifier}`, JSON.stringify(requestsWithinWindow));
+  }
+
   res.send(`Hello World!`);
 });
 
